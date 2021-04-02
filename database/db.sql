cbf5d184099bea900ec9867ecc0581b429dd375e
Leozin89Python <leozin069@gmail.com>
2021-04-02 02:03:03 -0300

    minhasfincas-version~1.1.1

---
+++ database/db.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA financas;
+
+DROP SCHEMA financas;
+
+CREATE TABLE financas.usuario(
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	nome CHARACTER VARYING(150),
+	email CHARACTER VARYING(100),
+	senha CHARACTER VARYING(20),
+	data_cadastro DATE DEFAULT NOW()
+);
+
+DROP TABLE financas.usuario;
+
+
+
+CREATE TABLE financas.lancamento(
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	descricao CHARACTER VARYING(100) NOT NULL,
+	mes INTEGER NOT NULL,
+	ano INTEGER NOT NULL,
+	valor NUMERIC(16, 2)NOT NULL,
+	tipo CHARACTER VARYING(20) CHECK( tipo IN ('RECEITA','DESPESA') )  NOT NULL,
+	status CHARACTER VARYING(20)  CHECK( status IN ('PENDENTE','CANCELADO','EFETIVADO') ) NOT NULL,
+	id_usuario BIGINT REFERENCES financas.usuario(id) NOT NULL,
+	data_cadastro DATE DEFAULT NOW()
+);
+
+DROP TABLE financas.lancamento
+
+
+SELECT * FROM financas.usuario;
+SELECT * FROM financas.lancamento;
